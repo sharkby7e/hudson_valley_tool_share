bcfdd3f1f1d941dbfa6f2f8138f97ef7e266efc3
sharkby7e <sgquin@gmail.com>
2024-09-28 10:23:21 -0400

    setup database for separate local and prod

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,5 +1,18 @@
-import { drizzle } from "drizzle-orm/vercel-postgres";
-import * as schema from "./schema";
 import { sql } from "@vercel/postgres";
+import postgres from "postgres";
+import { drizzle as VercelDrizzle } from "drizzle-orm/vercel-postgres";
+import {
+  drizzle as LocalDrizzle,
+  type PostgresJsDatabase,
+} from "drizzle-orm/postgres-js";
+import * as schema from "./schema";
 
-export const db = drizzle(sql, { schema });
+let db;
+if (process.env.NODE_ENV === "production") {
+  db = VercelDrizzle(sql, { schema });
+} else {
+  const migrationClient = postgres(process.env.POSTGRES_URL as string);
+  db = LocalDrizzle(migrationClient, { schema });
+}
+
+export { db };
