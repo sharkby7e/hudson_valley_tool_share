8fb16aadf3d73dd4eee0d0f5c8813f66098cc7b1
sharkby7e <sgquin@gmail.com>
2024-09-29 00:02:02 -0400

    add a link to the tools page to see if thats the problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,14 +20,9 @@ export default async function Home() {
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
+              href="/tools"
             >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
+              <h3 className="text-2xl font-bold">Tools →</h3>
             </Link>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
