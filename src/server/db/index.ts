8c2c1267178478efa1b288f8a13ad52f233cd1e6
sharkby7e <sgquin@gmail.com>
2024-09-28 23:08:09 -0400

    set types for db

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,18 +1,21 @@
 import { sql } from "@vercel/postgres";
 import postgres from "postgres";
-import { drizzle as VercelDrizzle } from "drizzle-orm/vercel-postgres";
+import {
+  drizzle as VercelDrizzle,
+  type VercelPgDatabase,
+} from "drizzle-orm/vercel-postgres";
 import {
   drizzle as LocalDrizzle,
   type PostgresJsDatabase,
 } from "drizzle-orm/postgres-js";
 import * as schema from "./schema";
 
-let db;
+let db: VercelPgDatabase<typeof schema> | PostgresJsDatabase<typeof schema>;
 if (process.env.NODE_ENV === "production") {
   db = VercelDrizzle(sql, { schema });
 } else {
-  const migrationClient = postgres(process.env.POSTGRES_URL as string);
-  db = LocalDrizzle(migrationClient, { schema });
+  const queryClient = postgres(process.env.POSTGRES_URL as string);
+  db = LocalDrizzle(queryClient, { schema });
 }
 
 export { db };
